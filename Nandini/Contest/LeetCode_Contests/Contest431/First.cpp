7bfe81a24823e10e148f7b6608c0dd20a7dde32f
Nandini Jindal <nandinijindal010@gmail.com>
2025-01-06 07:43:00 +0530

    first

---
+++ Nandini/Contest/LeetCode_Contests/Contest431/First.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxLength(vector<int>& nums) {
+       int n = nums.size(),j,ans=0;
+       for(int i=0 ;i<n ;i++)  
+       {
+         int gcdd = nums[i], lcmm = nums[i];
+         long long pdt = nums[i];
+         for( j=i+1 ; j<n ; j++)
+         {
+            gcdd = __gcd(gcdd,nums[j]);
+            lcmm = lcm(lcmm,nums[j]);
+            pdt = pdt*nums[j];
+            // cout << "lcm=" << lcmm << ",gcd=" << gcdd << ",pdt=" << pdt << endl;
+            if(pdt!=(long long)gcdd*lcmm) break;
+         }
+         ans = max(ans,j-i);
+       } 
+       return ans;
+    }
+};
