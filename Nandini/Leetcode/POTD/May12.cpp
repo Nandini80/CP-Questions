98169c201c3db19d7a45dc6239b461e32ccff1a5
Nandini Jindal <nandinijindal010@gmail.com>
2025-06-12 08:54:42 +0530

    may 12

---
+++ Nandini/Leetcode/POTD/May12.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxAdjacentDistance(vector<int>& nums) {
+        int n = nums.size();
+        int diff = abs(nums[0]-nums[n-1]);
+        for(int i=0 ;i <n-1 ; i++){
+            diff = max(diff,abs(nums[i]-nums[i+1]));
+        }
+        return diff;
+    }
+};
